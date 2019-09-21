399dc8e7955f0ace7781514c047cbaa57a976851
PermanentPortionX <(?jkFQWJ?8aPWPM4>
2019-09-21 19:45:41 +0200

    tested MainActivity

--- app/src/main/java/com/wits/project/witsfreelancing/MainActivity.java
+++ app/src/main/java/com/wits/project/witsfreelancing/MainActivity.java
@@ -2,6 +2,7 @@ package com.wits.project.witsfreelancing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView browser = findViewById(R.id.webView);
+        loadHomePage(browser);
     }
+
+    public static void loadHomePage(WebView browser){
+        browser.loadUrl("www/html/index.html");
+    }
+
 }
